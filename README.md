6ac60618184976ed7198a060b0341508af90a28e
suchz01 <110681765+suchz01@users.noreply.github.com>
2025-03-09 03:52:27 +0000

    basic-tailwind-setup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# todo.app
+To install all dependencies for the project (root, frontend, and backend), run:
+```sh
+npm run ins
+```
+
+To run both backend and frontend, run:
+```sh
+npm run dev
+```
---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
